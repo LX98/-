2e271dd33a06f4eadfa3e13a26e73d1860e93b6d
LX98 <44269544+LX98@users.noreply.github.com>
2019-10-16 23:59:08 +0800

    Update 已知树的后序遍历和中序遍历的C++算法实现.cpp

--- 已知树的后序遍历和中序遍历的C++算法实现.cpp
+++ 已知树的后序遍历和中序遍历的C++算法实现.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 int N;
-int preorder[30];
-int inorder[30];
-int postorder[30];
+int preorder[30];	// 先序遍历数组
+int inorder[30];	// 中序遍历数组
+int postorder[30];	// 后序遍历数组
 
 
-void Travel(int left,int right,int pleft,int pright)
+void Travel(int left,int right,int pleft,int pright)	// 这里分别是一棵树在中序、后序遍历数组中的范围
 {
 	static int index = 0;
-	if (left > right)
+	if (left > right) // left==right时，说明这棵树只有一个节点
 		return;
 
 
-	int root = postorder[pright];
-	preorder[index++] = root;
+	int root = postorder[pright];	// 从后序遍历数组中找到这个树的根
+	preorder[index++] = root;		// 因为是先序遍历，访问这棵树时，先把根记录下来
 	for (int i = left; i <= right; i++)
 	{
-		if (inorder[i] == root)
+		if (inorder[i] == root)		// 从中序遍历数组中定位树的根，然后根的左右分别是它的左右子树
 		{
-			Travel(left, i - 1,pleft,i-left - 1+pleft);
+			Travel(left, i - 1,pleft,i-left - 1+pleft);// 左子树   i - 1 - left，左子树的长度
 			Travel(i + 1, right, i - left + pleft,pright-1);
 			break;
 		}
